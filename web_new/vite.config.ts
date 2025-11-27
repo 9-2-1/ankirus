f292ab3983a158711c827ba95b9baadc0a057f05
arigi <arigi@arigi.top>
2025-11-27 18:50:41 +0800

    类型检查，构建结果

--- web_new/vite.config.ts
+++ web_new/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react({ babel: { plugins: ['babel-plugin-react-compiler'] } })],
   server: { proxy: { '/cards': { target: 'http://localhost:24032', changeOrigin: true } } },
 });
